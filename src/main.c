40cd30dbc21bbae554784704ed1a091cadac3566
Lukasz Witkowski <witul@linux.pl>
2017-11-01 07:35:41 +0100

    power off test

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include <avr/pgmspace.h>
 #include <avr/eeprom.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/atomic.h>
 
 #define DEBOUNCE 2000
 
@@ -15,7 +17,8 @@
 #define COLORS 7
 
 
-volatile uint8_t f=0;
+volatile uint8_t f=1;
+volatile uint8_t sm=0;
 
 static uint8_t EEMEM initial_dim=2;
 static uint8_t EEMEM initial_color=3;
@@ -42,14 +45,14 @@ static const uint8_t _brightnessAdj[] PROGMEM = {
 static const uint8_t _dimmingAdj[] PROGMEM = {25,100,175,255};
 
 static const uint8_t colors[COLORS][3] PROGMEM = {
-    {0,255,0},
     //    {0,0,255},
+    {255,0,0},
     {255,130,50},
     {255,255,153},
     {255,204,153},
     {204,255,204},
     {255,204,102},
-    {255,0,0}
+    {0,255,0}
 };
 
 
@@ -78,8 +81,14 @@ static void set_dim(const PGM_VOID_P colorPtr,uint8_t step){
     OCR1B =  calculateColor(pgm_read_byte(colorPtr),step);
 }
 
-uint8_t dim=3;
+static void pwmOff(){
+    OCR0A =0; 
+    OCR1A =0;
+    OCR1B =0;
+}
+volatile uint8_t dim=3;
 uint8_t color=0;
+
 const PGM_VOID_P colorsPtr;
 PGM_VOID_P ptr;
 
@@ -95,9 +104,15 @@ static void updateLed(){
 }
 
 static void nextColor(){
-    if(ptr==colorsPtr+3*(COLORS-1))
+    if(ptr==colorsPtr+3*(COLORS-1)){
         ptr=colorsPtr;
-    else ptr+=3;
+        color=0;
+    }
+    else {
+        ptr+=3;
+        color++;
+    }
+
 }
 
 static void nextDim(){
@@ -108,6 +123,8 @@ static void nextDim(){
 
 void main(){
 
+
+
     DDRD = 0x00;
     PORTD = 0xFF;
 
@@ -115,7 +132,8 @@ void main(){
     PORTB |=(1<<PB0)|(1<<PB1);
 
     DDRB |= (1<<PB2) | (1<<PB4) | (1<<PB3);
-    PORTB |= (1<<PB2) | (1<<PB4) | (1<<PB3);
+    PORTB=0;
+    //&=~((1<<PB2) | (1<<PB4) | (1<<PB3));
 
     TCCR0A |= (1<<WGM00) | (1<<COM0A1);
     TCCR0B |= (1<<CS00);
@@ -123,70 +141,105 @@ void main(){
     TCCR1A |= (1<<WGM10) | (1<<COM1A1) | (1<<COM1B1);
     TCCR1B |= (1<<CS10);
 
-    //eeprom_write_byte(&initial_dim,2);
-    //eeprom_write_byte(&initial_color,3);
+    OCR0B = 250;
 
     TIMSK |= (1<<OCIE0B);
+
     //power button
-    GIMSK |= (1<<INT0);
-    //        |(1<<PCIE2);                 // Enable INT0
-    MCUCR |= (1<<ISC00)|(1<<ISC01);    // Trigger INT0 on rising edge
+    GIMSK |= (1<<INT0)|(1<<INT1); // Enable INT0
+
+    //MCUCR |= (1<<ISC00)|(1<<ISC01);
+    MCUCR &= ~((1<<ISC00)|(1<<ISC01));    // Trigger INT0 on level change
+    
+    // GIFR |=(1<<INTF0)|(1<<INTF1);
+
+    //GIMSK |=        |(1<<PCIE2);                 
     //PCMSK2|=(1<<PCINT15)|(1<<PCINT14);
-    //GIFR |=(1<<INTF0);
-    OCR0B = 250;
+    //
     sei();
 
     colorsPtr=pgm_get_far_address(colors[0]);
     ptr=colorsPtr;
 
-    if(eeprom_is_ready()){
-        dim=eeprom_read_byte(&initial_dim);
-        color=eeprom_read_byte(&initial_color);
-        ptr=colorsPtr+(3*color);
+    uint8_t _in_dim=3;
+    uint8_t _in_color=5;
+ /*   if(eeprom_is_ready()){
+        _in_dim=eeprom_read_byte(&initial_dim);
+        _in_color=eeprom_read_byte(&initial_color);
     }
+   */ 
+    color=_in_color;
+    ptr=colorsPtr+(3*color);
+    dim=_in_dim;
 
-    ptr=colorsPtr+(3*5);
-    dim=3;
+//    ptr=colorsPtr+(3*5);
+
     updateLed();
 
 
-    //uint8_t i=0,j=0;
-
+    uint8_t i=0,j=0;
     uint8_t s=0;
-    while (1)
-    {
+    while (1) {
+
         if(f!=s){
-            if(f==1){
-                updateLed();
-            }
-            else{
-                OCR0A = 0;
-                //zielony
-                OCR1A =0;
-                //czerwony
-                OCR1B =0;
-            }
+            //  if(f==1){
+            i++;
+            nextColor();
+            updateLed();
+            //  }
             s=f;
         }
-        //_delay_ms(100);
-        /*
-        for(i=0;i<COLORS;i++){
-            dim=0;
-            for(j=0;j<DIM_STEPS;j++){
-                _delay_ms(1000);
-                updateLed();
-                nextDim();
-            }
-            nextColor();
-        }*/
+
+        if(sm==1){
+        cli();
+            sm=3;
+
+         //   while(!(PIND & (1<<PD2))){
+         //   }
+
+            set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+            i=0;
+            sleep_enable();
+/*
+            if(dim!=_in_dim)
+                eeprom_write_byte(&initial_dim,dim);
+            if(color!=_in_color)
+                eeprom_write_byte(&initial_color,color);
+*/
+            DDRB=0;
+            PORTB=0;
+            pwmOff();
+
+   //         MCUCR &= ~((1<<ISC00)|(1<<ISC01));    // Trigger INT0 on level change
+
+            sei();
+            sleep_cpu();
+            sleep_disable();
+            sm=0;
+            DDRB=255;
+   //         MCUCR |= (1<<ISC00)|(1<<ISC01);
+        }
     }
 }
+
 
 ISR(TIMER0_COMPB_vect){
     static uint16_t t=0;
     t++;
     if(t>60000){
-        f=f==0?1:0;
+        f=f==0?1:0;      
         t=0;
-   }
+    }
 }
+
+
+ISR(INT0_vect){
+    static uint8_t m=0;
+    if(sm==0){
+        m=1;
+        sm=1;
+    }
+    else{m=0;sm=0;}
+}
+
